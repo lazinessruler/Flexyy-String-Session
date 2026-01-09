12dedef2795df5434e48dc85e9428dbb84532d6e
𝖸𝖺𝗁𝗂𝗄𝗈 !! <lazinessruler@gmail.com>
2026-01-09 17:44:44 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -9,5 +9,14 @@ API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "8561360797"))
 
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/lazinessruler/Flexyy-String-Session",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+) 
+
 MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN", "ScriptFlix_Bots")
